e60646d138d9340855ca6ae124560de7ab321506
Daniel Cosme <pipecosme123@gmail.com>
2021-03-01 19:58:07 -0500

    Inicio de JavaScript
    Se creo una carpeta y un archivo JavaScript y se escribio una linea de codigo en el HTML haciendo referencia al archivo .js

---
+++ JS/script.js
@@ -0,0 +1,12 @@
+var num1 = 1;
+var num2 = 2;
+
+/**
+ * 
+ * @param {*} number1 
+ * @param {*} number2 
+ */
+function sumar(number1,number2){
+    var resultado = number1 + number2;
+    return resultado;
+}
