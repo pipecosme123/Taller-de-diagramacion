fbb5e7bdb404e0254cdcfb52f52072d17600e97a
Daniel Cosme <pipecosme123@gmail.com>
2021-03-08 17:50:49 -0500

    Funcion de la calculadora terminada

--- JS/script.js
+++ JS/script.js
@@ -10,3 +10,26 @@ function sumar(number1,number2){
     var resultado = number1 + number2;
     return resultado;
 }
+
+/**
+ * 
+ * @param {int} num1 
+ * @param {int} num2 
+ * @param {string} op 
+ * @returns int
+ * Esta funcion calcula una operacion basica matematica (suma, resta, multiplicacion, division) entre dos numeros
+ */
+function calcular(num1, num2, op){
+    var resultado;
+
+    if(op == "suma"){
+        resultado = num1 + num2;
+    }else if(op == "resta"){
+        resultado = num1 - num2;
+    }else if(op == "multip"){
+        resultado = num1 * num2;
+    }else{
+        resultado = num1 / num2;
+    }
+    return resultado;
+}
