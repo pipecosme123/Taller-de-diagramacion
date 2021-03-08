0bf9e5c8529f2262a83b1b444c996bb2d4055341
Daniel Cosme <pipecosme123@gmail.com>
2021-03-08 18:41:30 -0500

    Cambios

--- JS/script.js
+++ JS/script.js
@@ -20,16 +20,19 @@ function sumar(number1,number2){
  * Esta funcion calcula una operacion basica matematica (suma, resta, multiplicacion, division) entre dos numeros
  */
 function calcular(num1, num2, op){
-    var resultado;
 
     if(op == "suma"){
-        resultado = num1 + num2;
+        var resultado = num1 + num2;
+        return resultado;
     }else if(op == "resta"){
-        resultado = num1 - num2;
+        var resultado = num1 - num2;
+        return resultado;
     }else if(op == "multip"){
-        resultado = num1 * num2;
+        var resultado = num1 * num2;
+        return resultado;
     }else{
-        resultado = num1 / num2;
+        var resultado = num1 / num2;
+        return resultado;
     }
-    return resultado;
+
 }
